c3ca3619e696d0be3266cefccebbab410df34e29
AbdulazizKarimli <abdulaziz.karimli@code.edu.az>
2022-07-03 12:40:56 +0400

    user model is added

---
+++ SproutSocial/SproutSocial.Core/Entities/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SproutSocial.Core.Entities
+{
+    public class AppUser : IdentityUser
+    {
+        public string? Fullname { get; set; }
+        public string? ProfilePhoto { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
