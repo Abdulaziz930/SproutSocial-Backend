989d28ef505c74e22d37be96b906d82630ff8b5f
AbdulazizKarimli <abdulaziz.karimli@code.edu.az>
2022-07-03 15:45:31 +0400

    feature: data initializer is added

---
+++ SproutSocial/SproutSocial.Data/Identity/DataInitializer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SproutSocial.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SproutSocial.Data.Identity
+{
+    public class DataInitializer
+    {
+        public static void SeedData(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
+        {
+            foreach (var role in Enum.GetValues(typeof(RoleConstants.RoleType)))
+            {
+                var isExist = roleManager.RoleExistsAsync(role.ToString()).Result;
+                if (!isExist)
+                {
+                    roleManager.CreateAsync(new IdentityRole { Name = role.ToString() }).Wait();
+                }
+            }
+
+            var user = new AppUser
+            {
+                Fullname = "Admin",
+                UserName = "Admin",
+                Email = "admin@gmail.com",
+                IsActive = true
+            };
+
+            var existUser = userManager.FindByNameAsync("Admin").Result;
+            if (existUser == null)
+            {
+                userManager.CreateAsync(user, "Admin@123").Wait();
+                userManager.AddToRoleAsync(user, RoleConstants.RoleType.Admin.ToString()).Wait();
+            }
+        }
+    }
+}
