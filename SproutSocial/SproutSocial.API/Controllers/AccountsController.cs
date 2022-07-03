0000960d2e1b6fa5e3a3690f7ea7884d5bca88b3
AbdulazizKarimli <abdulaziz.karimli@code.edu.az>
2022-07-04 02:10:03 +0400

    feature: account controller and register action is added

---
+++ SproutSocial/SproutSocial.API/Controllers/AccountsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SproutSocial.Service.Dtos;
+using SproutSocial.Service.Dtos.Account;
+using SproutSocial.Service.Services.Interfaces;
+
+namespace SproutSocial.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountsController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            await _accountService.RegisterAsync(registerDto);
+
+            return Ok(new ResponseDto { Status = StatusCodes.Status201Created, Message = "User successfully registered" });
+        }
+    }
+}
