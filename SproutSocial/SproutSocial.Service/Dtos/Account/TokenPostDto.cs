45f8f6dbc4b4995b4e9ef5bf41d567c6def65f5e
AbdulazizKarimli <abdulaziz.karimli@code.edu.az>
2022-07-04 04:29:02 +0400

    feature: tokenPostDto is added

---
+++ SproutSocial/SproutSocial.Service/Dtos/Account/TokenPostDto.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SproutSocial.Service.Dtos.Account
+{
+    public class TokenPostDto
+    {
+        public string? AccessToken { get; set; }
+        public string? RefreshToken { get; set; }
+    }
+
+    public class TokenPostValidator : AbstractValidator<TokenPostDto>
+    {
+        public TokenPostValidator()
+        {
+            RuleFor(x => x.AccessToken).NotEmpty().NotNull();
+            RuleFor(x => x.RefreshToken).NotEmpty().NotNull();
+        }
+    }
+}
