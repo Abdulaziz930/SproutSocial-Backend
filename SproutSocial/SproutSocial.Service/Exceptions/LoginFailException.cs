309a7076073dbe87a04febb457cac88ccfadc845
AbdulazizKarimli <abdulaziz.karimli@code.edu.az>
2022-07-04 04:31:17 +0400

    feature: login fail exception is added

---
+++ SproutSocial/SproutSocial.Service/Exceptions/LoginFailException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SproutSocial.Service.Exceptions
+{
+    public class LoginFailException : Exception
+    {
+        public LoginFailException(string message) : base(message)
+        {
+        }
+    }
+}
