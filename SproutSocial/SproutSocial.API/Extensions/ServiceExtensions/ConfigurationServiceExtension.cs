6e613d84fe95fff4cd9d0d446baa1881c9948449
AbdulazizKarimli <abdulaziz.karimli@code.edu.az>
2022-07-07 00:45:17 +0400

    feature: project configuration service extension is created

---
+++ SproutSocial/SproutSocial.API/Extensions/ServiceExtensions/ConfigurationServiceExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SproutSocial.API.Extensions.ServiceExtensions
+{
+    public static class ConfigurationServiceExtension
+    {
+        public static void AddConfigureService(this IServiceCollection services)
+        {
+            services.Configure<DataProtectionTokenProviderOptions>(options =>
+                options.TokenLifespan = TimeSpan.FromMinutes(15));
+        }
+    }
+}
