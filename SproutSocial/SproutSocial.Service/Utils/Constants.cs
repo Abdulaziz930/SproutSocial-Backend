df876e6850f02ed47f73e7dba233c650c4b10d05
AbdulazizKarimli <abdulaziz.karimli@code.edu.az>
2022-07-07 00:18:41 +0400

    feature: constant settings is added

---
+++ SproutSocial/SproutSocial.Service/Utils/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SproutSocial.Service.Utils
+{
+    public static class Constants
+    {
+        public static string EmailAddress = "";
+        public static string EmailPassword = "";
+    }
+}
