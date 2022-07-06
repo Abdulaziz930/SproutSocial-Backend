8558718a00c212a26f36de54c17110da4808e605
AbdulazizKarimli <abdulaziz.karimli@code.edu.az>
2022-07-07 00:20:28 +0400

    feature: constant settings register service is created

---
+++ SproutSocial/SproutSocial.API/Extensions/ServiceExtensions/ConstantExtension.cs
@@ -0,0 +1,14 @@
+﻿
+using SproutSocial.Service.Utils;
+
+namespace SproutSocial.API.Extensions.ServiceExtensions
+{
+    public static class ConstantExtension
+    {
+        public static void AddConstants(this IServiceCollection services, IConfiguration configuration)
+        {
+            Constants.EmailAddress = configuration["Gmail:Address"];
+            Constants.EmailPassword = configuration["Gmail:Password"];
+        }
+    }
+}
