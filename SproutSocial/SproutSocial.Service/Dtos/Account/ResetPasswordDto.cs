2c6e293383578bd20b38135bc5f44263b769f39f
AbdulazizKarimli <abdulaziz.karimli@code.edu.az>
2022-07-07 00:26:17 +0400

    feature: reset password dto is added

---
+++ SproutSocial/SproutSocial.Service/Dtos/Account/ResetPasswordDto.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SproutSocial.Service.Dtos.Account
+{
+    public class ResetPasswordDto
+    {
+        public string? Token { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+    }
+
+    public class ResetPasswordDtoValidator : AbstractValidator<ResetPasswordDto>
+    {
+        public ResetPasswordDtoValidator()
+        {
+            RuleFor(x => x.Token).NotEmpty().NotNull();
+            RuleFor(x => x.Password).NotEmpty().NotNull().MinimumLength(8).MaximumLength(100);
+            RuleFor(x => x.ConfirmPassword).NotEmpty().NotNull().MinimumLength(8).MaximumLength(100).Equal(x => x.Password);
+        }
+    }
+}
