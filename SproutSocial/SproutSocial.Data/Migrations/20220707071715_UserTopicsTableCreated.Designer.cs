ab26f18840a7605f790132339b2be66759b7ae7b
AbdulazizKarimli <abdulaziz.karimli@code.edu.az>
2022-07-07 12:06:52 +0400

    feature: userTopic migrations is added

--- SproutSocial/SproutSocial.Data/Migrations/20220707071715_UserTopicsTableCreated.Designer.cs
+++ SproutSocial/SproutSocial.Data/Migrations/20220707071715_UserTopicsTableCreated.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SproutSocial.Data;
 
@@ -11,9 +12,10 @@ using SproutSocial.Data;
 namespace SproutSocial.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220707071715_UserTopicsTableCreated")]
+    partial class UserTopicsTableCreated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
