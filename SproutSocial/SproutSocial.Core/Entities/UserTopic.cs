b9e3a9371387aad60030baad9ce9162c693630e2
AbdulazizKarimli <abdulaziz.karimli@code.edu.az>
2022-07-07 12:03:12 +0400

    feature: userTopic model is added

---
+++ SproutSocial/SproutSocial.Core/Entities/UserTopic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SproutSocial.Core.Entities
+{
+    public class UserTopic
+    {
+        public int Id { get; set; }
+        public string? UserId { get; set; }
+        public AppUser User { get; set; }
+        public int TopicId { get; set; }
+        public Topic Topic { get; set; }
+    }
+}
