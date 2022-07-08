434ae9ddfaf6306153f26f21bf0b5859f1929a92
AbdulazizKarimli <abdulaziz.karimli@code.edu.az>
2022-07-08 16:44:14 +0400

    feature: blog model is created

---
+++ SproutSocial/SproutSocial.Core/Entities/Blog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SproutSocial.Core.Entities
+{
+    public class Blog : BaseEntity
+    {
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public string? Image { get; set; }
+        public string? UserId { get; set; }
+        public AppUser? User { get; set; }
+    }
+}
