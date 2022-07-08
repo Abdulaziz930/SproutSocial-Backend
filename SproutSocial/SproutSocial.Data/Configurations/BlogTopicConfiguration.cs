58528b630ab0d6855eb8ec577fa44c091645d6ea
AbdulazizKarimli <abdulaziz.karimli@code.edu.az>
2022-07-08 17:09:06 +0400

    feature: blogTopic model configurations is created

---
+++ SproutSocial/SproutSocial.Data/Configurations/BlogTopicConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SproutSocial.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SproutSocial.Data.Configurations
+{
+    public class BlogTopicConfiguration : IEntityTypeConfiguration<BlogTopic>
+    {
+        public void Configure(EntityTypeBuilder<BlogTopic> builder)
+        {
+            builder.HasOne(x => x.Blog).WithMany(x => x.BlogTopics).HasForeignKey(x => x.BlogId);
+            builder.HasOne(x => x.Topic).WithMany(x => x.BlogTopics).HasForeignKey(x => x.TopicId);
+        }
+    }
+}
