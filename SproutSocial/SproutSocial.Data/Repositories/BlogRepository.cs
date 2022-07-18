f087f9f2cf07a0f9a355fcfdb76e713d8fed876b
AbdulazizKarimli <abdulaziz.karimli@code.edu.az>
2022-07-18 21:42:44 +0400

    feature: blog repository is added

---
+++ SproutSocial/SproutSocial.Data/Repositories/BlogRepository.cs
@@ -0,0 +1,18 @@
+﻿using SproutSocial.Core.Entities;
+using SproutSocial.Core.Repositories;
+using SproutSocial.Data.Repositories.EFRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SproutSocial.Data.Repositories
+{
+    public class BlogRepository : Repository<Blog, AppDbContext>, IBlogRepository
+    {
+        public BlogRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
