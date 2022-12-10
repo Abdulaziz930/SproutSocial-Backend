39baa7b198768f3e196ec3ed3b510e3f4ffb2d68
Abdulaziz930 <abdulazizak@code.edu.az>
2022-12-11 02:25:20 +0400

    update background job

--- SproutSocial/src/Infrastructure/SproutSocial.Quartz/Configuration.cs
+++ SproutSocial/src/Infrastructure/SproutSocial.Quartz/Configuration.cs
@@ -4,6 +4,8 @@ namespace SproutSocial.Quartz;
 
 internal static class Configuration
 {
+    public static string BaseUrl { get => "http://localhost:1901/api/v1"; }
+
     internal static string GetCronExpression(string jobType)
     {
         ConfigurationManager configurationManager = new();
--- SproutSocial/src/Infrastructure/SproutSocial.Quartz/Jobs/TestJob.cs
+++ SproutSocial/src/Infrastructure/SproutSocial.Quartz/Jobs/TestJob.cs
@@ -1,13 +1,38 @@
-﻿using Quartz;
+﻿using Newtonsoft.Json;
+using Quartz;
+using SproutSocial.Application.DTOs.MailDtos;
 
 namespace SproutSocial.Quartz.Jobs;
 
 [DisallowConcurrentExecution]
 public class TestJob : IJob
 {
-    public Task Execute(IJobExecutionContext context)
+    public async Task Execute(IJobExecutionContext context)
     {
-        Console.WriteLine("test job executed");
-        return Task.CompletedTask;
+        using (HttpClient client = new())
+        {
+            MailRequestDto mailRequestDto = new();
+            mailRequestDto.ToEmail = "karimliabdulaziz5@gmail.com";
+            mailRequestDto.Subject = "TestSubject";
+            mailRequestDto.Body = "TestBody";
+
+            var formContent = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("ToEmail", mailRequestDto.ToEmail),
+                new KeyValuePair<string, string>("Subject", mailRequestDto.Subject),
+                new KeyValuePair<string, string>("Body", mailRequestDto.Body)
+            });
+
+            var response = await client.PostAsync($"{Configuration.BaseUrl}/Mail", formContent);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseStr = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseStr);
+            }
+            else
+            {
+                Console.WriteLine("Problem occurred when sending email");
+            }
+        }
     }
 }
