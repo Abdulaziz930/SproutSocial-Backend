b203c448241c68ddd896956ea97b65088a2a06e3
Abdulaziz930 <karimliabdulaziz5@gmail.com>
2023-05-16 18:34:17 +0400

    add confirm email dtos

---
+++ SproutSocial/src/Core/SproutSocial.Application/DTOs/UserDtos/ConfirmEmailDto.cs
@@ -0,0 +1,3 @@
+﻿namespace SproutSocial.Application.DTOs.UserDtos;
+
+public record ConfirmEmailDto(string Token, string Email);
---
+++ SproutSocial/src/Core/SproutSocial.Application/DTOs/UserDtos/ConfirmEmailResponseDto.cs
@@ -0,0 +1,3 @@
+﻿namespace SproutSocial.Application.DTOs.UserDtos;
+
+public record ConfirmEmailResponseDto(bool IsSuccess, string Message);
