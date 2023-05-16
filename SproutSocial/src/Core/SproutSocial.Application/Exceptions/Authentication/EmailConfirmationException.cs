7782d537f2b399ad5e17b398b3cf5ab5ef89b511
Abdulaziz930 <karimliabdulaziz5@gmail.com>
2023-05-16 18:34:59 +0400

    add emailConfirmationException

---
+++ SproutSocial/src/Core/SproutSocial.Application/Exceptions/Authentication/EmailConfirmationException.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SproutSocial.Application.Exceptions.Authentication;
+
+public sealed class EmailConfirmationException : Exception, IServiceException
+{
+    public HttpStatusCode StatusCode => HttpStatusCode.BadRequest;
+
+    public string ErrorMessage { get; }
+    public string? ErrorDetail { get; }
+
+    public EmailConfirmationException()
+    {
+        ErrorMessage = "Unexpected error occurred while activate user";
+    }
+
+    public EmailConfirmationException(IEnumerable<IdentityError> errors)
+    {
+        ErrorMessage = "Unexpected error occurred while activate user";
+        ErrorDetail = String.Join(',', errors.Select(e => e.Description));
+    }
+
+    public EmailConfirmationException(string message) : base(message)
+    {
+        ErrorMessage = message;
+    }
+
+    public EmailConfirmationException(string message, Exception? innerException) : base(message, innerException)
+    {
+        ErrorMessage = message;
+    }
+}
