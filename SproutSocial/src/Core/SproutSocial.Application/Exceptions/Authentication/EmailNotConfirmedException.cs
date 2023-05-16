d8df8652b4599a47af17c5c9d7ad662f82798d90
Abdulaziz930 <karimliabdulaziz5@gmail.com>
2023-05-16 15:35:35 +0400

    feature: add EmailNotConfirmedException

---
+++ SproutSocial/src/Core/SproutSocial.Application/Exceptions/Authentication/EmailNotConfirmedException.cs
@@ -0,0 +1,28 @@
+﻿namespace SproutSocial.Application.Exceptions.Authentication;
+
+public sealed class EmailNotConfirmedException : Exception, IServiceException
+{
+    public HttpStatusCode StatusCode { get; }
+
+    public string ErrorMessage { get; }
+    public string? ErrorDetail { get; }
+
+    public EmailNotConfirmedException()
+    {
+        StatusCode = HttpStatusCode.Forbidden;
+        ErrorMessage = "Your account not activated.";
+        ErrorDetail = "Please activate your account from email sent link.";
+    }
+
+    public EmailNotConfirmedException(string message, HttpStatusCode statusCode) : base(message)
+    {
+        StatusCode = statusCode;
+        ErrorMessage = message;
+    }
+
+    public EmailNotConfirmedException(string message, HttpStatusCode statusCode, Exception? innerException) : base(message, innerException)
+    {
+        StatusCode = statusCode;
+        ErrorMessage = message;
+    }
+}
