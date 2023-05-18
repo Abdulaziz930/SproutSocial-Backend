0df8fd1dc565a0f5526d095bb4179ff301168d6c
Abdulaziz930 <abdulazizak@code.edu.az>
2023-05-18 17:07:36 +0400

    add UserNotRegisteredGAuthException

---
+++ SproutSocial/src/Core/SproutSocial.Application/Exceptions/Authentication/UserNotRegisteredGAuthException.cs
@@ -0,0 +1,23 @@
+﻿namespace SproutSocial.Application.Exceptions.Authentication;
+
+public sealed class UserNotRegisteredGAuthException : Exception, IServiceException
+{
+    public HttpStatusCode StatusCode => HttpStatusCode.BadRequest;
+
+    public string ErrorMessage { get; }
+
+    public UserNotRegisteredGAuthException()
+    {
+        ErrorMessage = "This user is not registered in the Google authenticator app";
+    }
+
+    public UserNotRegisteredGAuthException(string message) : base(message)
+    {
+        ErrorMessage = message;
+    }
+
+    public UserNotRegisteredGAuthException(string message, Exception? innerException) : base(message, innerException)
+    {
+        ErrorMessage = message;
+    }
+}
