ba51bf43709486bf1757758255a2982d01d96ef4
Abdulaziz930 <abdulazizak@code.edu.az>
2023-05-19 00:38:59 +0400

    add enable 2fa email commands

---
+++ SproutSocial/src/Core/SproutSocial.Application/Features/Commands/AppUser/EnableTwoFa/EnableTwoFaCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using SproutSocial.Application.DTOs.UserDtos;
+
+namespace SproutSocial.Application.Features.Commands.AppUser.EnableTwoFa;
+
+public class EnableTwoFaCommandHandler : IRequestHandler<EnableTwoFaCommandRequest, EnableTwoFaCommandResponse>
+{
+    private readonly IAuthService _authService;
+    private readonly IMapper _mapper;
+
+    public EnableTwoFaCommandHandler(IAuthService authService, IMapper mapper)
+    {
+        _authService = authService;
+        _mapper = mapper;
+    }
+
+    public async Task<EnableTwoFaCommandResponse> Handle(EnableTwoFaCommandRequest request, CancellationToken cancellationToken)
+    {
+        EnableTwoFaDto enableTwoFaDto = _mapper.Map<EnableTwoFaDto>(request);
+
+        await _authService.EnableTwoFaAsync(enableTwoFaDto);
+
+        return new()
+        {
+            StatusCode = HttpStatusCode.OK,
+            Message = "Google authenticator 2FA enabled"
+        };
+    }
+}
