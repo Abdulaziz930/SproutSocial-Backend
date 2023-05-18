6024005dd9d3700deb65edbae3d5577312c911b5
Abdulaziz930 <abdulazizak@code.edu.az>
2023-05-19 00:37:36 +0400

    add 2fa email command request

---
+++ SproutSocial/src/Core/SproutSocial.Application/Features/Commands/AppUser/EnableTwoFa/EnableTwoFaCommandRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace SproutSocial.Application.Features.Commands.AppUser.EnableTwoFa;
+
+public class EnableTwoFaCommandRequest : IRequest<EnableTwoFaCommandResponse>
+{
+    public string Email { get; set; } = null!;
+    public string Code { get; set; } = null!;
+}
