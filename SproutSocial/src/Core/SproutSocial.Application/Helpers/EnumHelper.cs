e28c0bfbd6c9f18e001a32a1baf6040e8010bc19
Abdulaziz930 <abdulazizak@code.edu.az>
2023-05-19 01:57:14 +0400

    add email helper for display enum values

---
+++ SproutSocial/src/Core/SproutSocial.Application/Helpers/EnumHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SproutSocial.Application.Helpers;
+
+public static class EnumHelper
+{
+    public static string GetEnumDisplayName<T>(T enumValue)
+    {
+        var displayAttribute = typeof(T)
+            .GetField(enumValue.ToString())
+            .GetCustomAttributes(typeof(DisplayAttribute), false)
+            .SingleOrDefault() as DisplayAttribute;
+
+        return displayAttribute?.Name ?? enumValue.ToString();
+    }
+}
