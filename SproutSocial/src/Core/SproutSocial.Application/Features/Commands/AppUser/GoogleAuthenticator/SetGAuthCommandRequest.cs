6b35dc65f98eadddbed46980f46ce27a2c9b952a
Abdulaziz930 <abdulazizak@code.edu.az>
2023-05-18 17:13:24 +0400

    add set Google Authenticator command request

---
+++ SproutSocial/src/Core/SproutSocial.Application/Features/Commands/AppUser/GoogleAuthenticator/SetGAuthCommandRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace SproutSocial.Application.Features.Commands.AppUser.GoogleAuthenticator;
+
+public class SetGAuthCommandRequest : IRequest<SetGAuthCommandResponse>
+{
+    public string Email { get; set; } = null!;
+    public string Code { get; set; } = null!;
+}
