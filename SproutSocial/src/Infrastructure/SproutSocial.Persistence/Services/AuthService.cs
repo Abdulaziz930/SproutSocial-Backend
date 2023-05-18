60ca15c1af9ce1d91ddb0c4979ad91a5b57c71c5
Abdulaziz930 <abdulazizak@code.edu.az>
2023-05-18 17:10:03 +0400

    add google authenticator 2fa methods

--- SproutSocial/src/Infrastructure/SproutSocial.Persistence/Services/AuthService.cs
+++ SproutSocial/src/Infrastructure/SproutSocial.Persistence/Services/AuthService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Google.Authenticator;
+using Microsoft.AspNetCore.Identity;
 using SproutSocial.Application.Abstractions.Email;
 using SproutSocial.Application.Abstractions.Services;
 using SproutSocial.Application.Abstractions.Token;
@@ -9,6 +10,7 @@ using SproutSocial.Application.Exceptions.Authentication.Token;
 using SproutSocial.Application.Exceptions.Users;
 using SproutSocial.Application.Helpers.Extesions;
 using SproutSocial.Domain.Entities.Identity;
+using SproutSocial.Domain.Enums;
 using System.Net;
 
 namespace SproutSocial.Persistence.Services;
@@ -48,10 +50,13 @@ public class AuthService : IAuthService
         {
             if (user.TwoFactorEnabled)
             {
-                await _signInManager.PasswordSignInAsync(user, model.Password, false, true);
+                if (user.TwoFactorAuthMethod == TwoFactorAuthMethod.Email)
+                {
+                    await _signInManager.PasswordSignInAsync(user, model.Password, false, true);
 
-                string twoFaCode = await _userManager.GenerateTwoFactorTokenAsync(user, "Email");
-                await _mailService.SendEmailAsync(new MailRequestDto { ToEmail = user.Email, Subject = "2FA Code", Body = $"Here is your code: {twoFaCode}" });
+                    string twoFaCode = await _userManager.GenerateTwoFactorTokenAsync(user, "Email");
+                    await _mailService.SendEmailAsync(new MailRequestDto { ToEmail = user.Email, Subject = "2FA Code", Body = $"Here is your code: {twoFaCode}" });
+                }
 
                 return new() { RequiresTwoFactor = true };
             }
@@ -73,14 +78,14 @@ public class AuthService : IAuthService
         confirmEmailDto.Email.ThrowIfNullOrWhiteSpace(message: "Email cannot be null");
 
         var user = await _userManager.FindByEmailAsync(confirmEmailDto.Email);
-        if(user is null)
+        if (user is null)
             throw new UserNotFoundException($"User not found by email: {confirmEmailDto.Email}", HttpStatusCode.BadRequest);
 
         if (await _userManager.IsEmailConfirmedAsync(user))
             throw new EmailConfirmationException("This account already activated");
 
         var result = await _userManager.ConfirmEmailAsync(user, confirmEmailDto.Token);
-        if(!result.Succeeded)
+        if (!result.Succeeded)
             throw new EmailConfirmationException(result.Errors);
 
         return new(true, $"User successfully activated. Username: {user.UserName}");
@@ -89,7 +94,7 @@ public class AuthService : IAuthService
     public async Task<TokenResponseDto> RefreshTokenLoginAsync(string refreshToken)
     {
         AppUser? user = await _userManager.Users.FirstOrDefaultAsync(u => u.RefreshToken == refreshToken);
-        if(user is null)
+        if (user is null)
             throw new UserNotFoundException("User not found");
 
         if (user?.RefreshTokenEndDate > DateTime.UtcNow)
@@ -114,5 +119,68 @@ public class AuthService : IAuthService
         }
 
         throw new AuthenticationFailException("Invalid Code");
+    }
+
+    public async Task<string> GetGAuthSetup(string email)
+    {
+        var user = await _userManager.FindByEmailAsync(email);
+        if (user is null)
+            throw new UserNotFoundException($"User not found by email: {email}", HttpStatusCode.BadRequest);
+
+        var authenticatorKey = await _userManager.GetAuthenticationTokenAsync(user, "Google Authenticator", "2FA");
+        if (authenticatorKey == null)
+        {
+            authenticatorKey = await _userManager.GenerateUserTokenAsync(user, "Google Authenticator", "2FA");
+            await _userManager.SetAuthenticationTokenAsync(user, "Google Authenticator", "2FA", authenticatorKey);
+        }
+
+        TwoFactorAuthenticator tfa = new TwoFactorAuthenticator();
+
+        var setupInfo = tfa.GenerateSetupCode("SproutSocial", user.Email, authenticatorKey, false, 3);
+
+        return setupInfo.QrCodeSetupImageUrl;
+    }
+
+    public async Task SetGAuth(SetGAuthDto setGAuthDto)
+    {
+        var user = await _userManager.FindByEmailAsync(setGAuthDto.Email);
+        if (user is null)
+            throw new UserNotFoundException($"User not found by email: {setGAuthDto.Email}", HttpStatusCode.BadRequest);
+
+        var authenticatorKey = await _userManager.GetAuthenticationTokenAsync(user, "Google Authenticator", "2FA"); ;
+        if (authenticatorKey is null)
+            throw new UserNotRegisteredGAuthException();
+
+        var authenticator = new TwoFactorAuthenticator();
+
+        var isValidCode = authenticator.ValidateTwoFactorPIN(authenticatorKey, setGAuthDto.Code);
+        if (!isValidCode)
+            throw new AuthenticationFailException("Invalid Code");
+
+        await _userManager.SetTwoFactorEnabledAsync(user, true);
+
+        user.TwoFactorAuthMethod = TwoFactorAuthMethod.GoogleAuthenticator;
+        await _userManager.UpdateAsync(user);
+    }
+
+    public async Task<TokenResponseDto> GAuthLogin(TwoFaLoginDto twoFaLoginDto, int accessTokenLifeTime)
+    {
+        var user = await _userManager.FindByEmailAsync(twoFaLoginDto.Email);
+        if (user is null)
+            throw new UserNotFoundException($"User not found by email: {twoFaLoginDto.Email}", HttpStatusCode.BadRequest);
+
+        var authenticatorKey = await _userManager.GetAuthenticationTokenAsync(user, "Google Authenticator", "2FA"); ;
+        if (authenticatorKey is null)
+            throw new UserNotRegisteredGAuthException();
+
+        var authenticator = new TwoFactorAuthenticator();
+
+        var isValidCode = authenticator.ValidateTwoFactorPIN(authenticatorKey, twoFaLoginDto.Code);
+        if (!isValidCode)
+            throw new AuthenticationFailException("Invalid Code");
+
+        var tokenResponse = await _tokenHandler.CreateAccessTokenAsync(accessTokenLifeTime, user);
+        await _userService.UpdateRefreshToken(tokenResponse.RefreshToken, user, tokenResponse.Expiration, 2);
+        return tokenResponse;
     }
 }
