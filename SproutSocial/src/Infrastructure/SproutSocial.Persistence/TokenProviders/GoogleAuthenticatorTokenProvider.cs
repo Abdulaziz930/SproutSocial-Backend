d5614e4bcf1bf4d54e27516464fa20fb24381970
Abdulaziz930 <abdulazizak@code.edu.az>
2023-05-18 17:05:04 +0400

    add Google Authenticator token provider

---
+++ SproutSocial/src/Infrastructure/SproutSocial.Persistence/TokenProviders/GoogleAuthenticatorTokenProvider.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SproutSocial.Persistence.TokenProviders;
+
+public class GoogleAuthenticatorTokenProvider<TUser> : TotpSecurityStampBasedTokenProvider<TUser>
+where TUser : class
+{
+    // No need to implement anything, use the base class
+    public override Task<bool> CanGenerateTwoFactorTokenAsync(UserManager<TUser> manager, TUser user)
+    {
+        return Task.FromResult(true);
+    }
+}
