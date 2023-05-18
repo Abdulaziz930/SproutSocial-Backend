16870a89e85f618c073bb6850e951a55dcd06d71
Abdulaziz930 <abdulazizak@code.edu.az>
2023-05-18 17:16:31 +0400

    add google authenticator login commands

---
+++ SproutSocial/src/Core/SproutSocial.Application/Features/Commands/AppUser/GAuthLogin/GAuthLoginCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using SproutSocial.Application.DTOs.UserDtos;
+
+namespace SproutSocial.Application.Features.Commands.AppUser.GAuthLogin;
+
+public class GAuthLoginCommandHandler : IRequestHandler<GAuthLoginCommandRequest, GAuthLoginCommandResponse>
+{
+    private readonly IAuthService _authService;
+    private readonly IMapper _mapper;
+
+    public GAuthLoginCommandHandler(IAuthService authService, IMapper mapper)
+    {
+        _authService = authService;
+        _mapper = mapper;
+    }
+
+    public async Task<GAuthLoginCommandResponse> Handle(GAuthLoginCommandRequest request, CancellationToken cancellationToken)
+    {
+        TwoFaLoginDto twoFaLoginDto = _mapper.Map<TwoFaLoginDto>(request);
+
+        var result = await _authService.GAuthLogin(twoFaLoginDto, 2);
+
+        return new()
+        {
+            AccessToken = result.AccessToken,
+            Expiration = result.Expiration,
+            RefreshToken = result.RefreshToken
+        };
+    }
+}
