770467db08fc91c37de341daf36c46b5ba2e99ff
Abdulaziz930 <abdulazizak@code.edu.az>
2023-05-20 14:05:10 +0400

    add twoFaMethod model

---
+++ SproutSocial/src/Core/SproutSocial.Domain/Entities/Identity/TwoFaMethod.cs
@@ -0,0 +1,9 @@
+﻿using SproutSocial.Domain.Enums;
+
+namespace SproutSocial.Domain.Entities.Identity;
+
+public class TwoFaMethod : BaseEntity
+{
+    public TwoFactorAuthMethod TwoFactorAuthMethod { get; set; }
+    public ICollection<UserTwoFaMethod> UserTwoFaMethods { get; set; }
+}
