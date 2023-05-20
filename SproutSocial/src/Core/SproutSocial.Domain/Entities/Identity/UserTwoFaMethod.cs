bf9d80bf45feecd826f5ba1620f8233dd961949c
Abdulaziz930 <abdulazizak@code.edu.az>
2023-05-20 14:06:08 +0400

    add user 2fa method model

---
+++ SproutSocial/src/Core/SproutSocial.Domain/Entities/Identity/UserTwoFaMethod.cs
@@ -0,0 +1,10 @@
+﻿namespace SproutSocial.Domain.Entities.Identity;
+
+public class UserTwoFaMethod : BaseEntity
+{
+    public Guid UserId { get; set; }
+    public AppUser AppUser { get; set; }
+    public Guid TwoFaMethodId { get; set; }
+    public TwoFaMethod TwoFaMethod { get; set; }
+    public bool IsSelected { get; set; }
+}
