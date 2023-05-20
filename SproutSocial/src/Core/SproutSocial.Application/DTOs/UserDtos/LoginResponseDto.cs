5fa8d6e4cf7893bdaabe9f1c8fc63395ba924d4d
Abdulaziz930 <abdulazizak@code.edu.az>
2023-05-20 16:40:06 +0400

    update login response dto

--- SproutSocial/src/Core/SproutSocial.Application/DTOs/UserDtos/LoginResponseDto.cs
+++ SproutSocial/src/Core/SproutSocial.Application/DTOs/UserDtos/LoginResponseDto.cs
@@ -5,7 +5,7 @@ namespace SproutSocial.Application.DTOs.UserDtos;
 public class LoginResponseDto
 {
     public bool RequiresTwoFactor { get; set; }
-    public TwoFactorAuthMethod? TwoFactorAuthMethod { get; set; }
+    public TwoFactorAuthMethod TwoFactorAuthMethod { get; set; }
     public string? TwoFaSecurityToken { get; set; }
     public TokenResponseDto? TokenResponse { get; set; }
 }
