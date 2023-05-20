828421be7791567b21ba7169fa8265938b6b0fe7
Abdulaziz930 <abdulazizak@code.edu.az>
2023-05-20 14:07:41 +0400

    add 2fa method model configurations

---
+++ SproutSocial/src/Infrastructure/SproutSocial.Persistence/Configurations/TwoFaMethodConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SproutSocial.Domain.Entities.Identity;
+
+namespace SproutSocial.Persistence.Configurations;
+
+public class TwoFaMethodConfiguration : IEntityTypeConfiguration<TwoFaMethod>
+{
+    public void Configure(EntityTypeBuilder<TwoFaMethod> builder)
+    {
+        builder.ToTable("TwoFaMethods");
+        builder.Property(tfa => tfa.TwoFactorAuthMethod).IsRequired(true);
+    }
+}
---
+++ SproutSocial/src/Infrastructure/SproutSocial.Persistence/Configurations/UserTwoFaMethodConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SproutSocial.Domain.Entities.Identity;
+
+namespace SproutSocial.Persistence.Configurations;
+
+public class UserTwoFaMethodConfiguration : IEntityTypeConfiguration<UserTwoFaMethod>
+{
+    public void Configure(EntityTypeBuilder<UserTwoFaMethod> builder)
+    {
+        builder.Property(ut => ut.IsSelected)
+            .HasDefaultValue(false);
+        builder.Property(ut => ut.UserId)
+            .IsRequired(true);
+        builder.Property(ut => ut.TwoFaMethodId)
+            .IsRequired(true);
+        builder
+            .HasOne(ut => ut.TwoFaMethod)
+            .WithMany(t => t.UserTwoFaMethods)
+            .HasForeignKey(ut => ut.TwoFaMethodId);
+        builder
+            .HasOne(ut => ut.AppUser)
+            .WithMany(u => u.UserTwoFaMethods)
+            .HasForeignKey(ut => ut.UserId);
+    }
+}
