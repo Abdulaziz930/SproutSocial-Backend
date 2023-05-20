fc74baa9908041479ebd1ca2d5cce05adfb3ff45
Abdulaziz930 <abdulazizak@code.edu.az>
2023-05-20 16:41:05 +0400

    add 2fa type already registered exception

---
+++ SproutSocial/src/Core/SproutSocial.Application/Exceptions/Authentication/TwoFaTypeAlreadyRegisteredException.cs
@@ -0,0 +1,26 @@
+﻿using SproutSocial.Domain.Enums;
+
+namespace SproutSocial.Application.Exceptions.Authentication
+{
+    public sealed class TwoFaTypeAlreadyRegisteredException : Exception, IServiceException
+    {
+        public HttpStatusCode StatusCode => HttpStatusCode.BadRequest;
+
+        public string ErrorMessage { get; }
+
+        public TwoFaTypeAlreadyRegisteredException(TwoFactorAuthMethod twoFactorAuthMethod)
+        {
+            ErrorMessage = $"{EnumHelper.GetEnumDisplayName(twoFactorAuthMethod)} 2FA type already registered";
+        }
+
+        public TwoFaTypeAlreadyRegisteredException(string message) : base(message)
+        {
+            ErrorMessage = message;
+        }
+
+        public TwoFaTypeAlreadyRegisteredException(string message, Exception? innerException) : base(message, innerException)
+        {
+            ErrorMessage = message;
+        }
+    }
+}
