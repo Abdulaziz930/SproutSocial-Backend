9ea34a33d2e7037773566a7437387c14644474c4
Abdulaziz930 <abdulazizak@code.edu.az>
2023-05-21 13:16:19 +0400

    add select 2fa method commands

---
+++ SproutSocial/src/Core/SproutSocial.Application/Features/Commands/AppUser/SelectTwoFaMethod/SelectTwoFaMethodCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using SproutSocial.Application.DTOs.UserDtos;
+
+namespace SproutSocial.Application.Features.Commands.AppUser.SelectTwoFaMethod;
+
+public class SelectTwoFaMethodCommandHandler : IRequestHandler<SelectTwoFaMethodCommandRequest, SelectTwoFaMethodCommandResponse>
+{
+    private readonly IUserService _userService;
+    private readonly IMapper _mapper;
+
+    public SelectTwoFaMethodCommandHandler(IUserService userService, IMapper mapper)
+    {
+        _userService = userService;
+        _mapper = mapper;
+    }
+
+    public async Task<SelectTwoFaMethodCommandResponse> Handle(SelectTwoFaMethodCommandRequest request, CancellationToken cancellationToken)
+    {
+        SelectTwoFaMethodDto selectTwoFaMethodDto = _mapper.Map<SelectTwoFaMethodDto>(request);
+
+        var result = await _userService.SelectTwoFaMethodAsync(selectTwoFaMethodDto);
+
+        return new()
+        {
+            StatusCode = result.StatusCode,
+            Message = result.Message
+        };
+    }
+}
---
+++ SproutSocial/src/Core/SproutSocial.Application/Features/Commands/AppUser/SelectTwoFaMethod/SelectTwoFaMethodCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿namespace SproutSocial.Application.Features.Commands.AppUser.SelectTwoFaMethod;
+
+public class SelectTwoFaMethodCommandValidator : AbstractValidator<SelectTwoFaMethodCommandRequest>
+{
+    public SelectTwoFaMethodCommandValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotNull().WithMessage("User id is required")
+            .NotEmpty().WithMessage("User id cannot be empty")
+            .Custom((id, context) =>
+            {
+                if (!Validators.IsGuid(id))
+                    context.AddFailure("Please enter correct User id");
+            });
+
+        RuleFor(x => x.TwoFaMethodId)
+            .NotNull().WithMessage("2FA method id is required")
+            .NotEmpty().WithMessage("2FA method id cannot be empty")
+            .Custom((id, context) =>
+            {
+                if (!Validators.IsGuid(id))
+                    context.AddFailure("Please enter correct 2FA method id");
+            });
+    }
+}
