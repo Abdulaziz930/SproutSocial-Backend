5e170148965a6e6ebb1485c07b64ecf9f929afce
Abdulaziz930 <abdulazizak@code.edu.az>
2023-05-21 13:15:52 +0400

    add select 2fa method mappers

---
+++ SproutSocial/src/Infrastructure/SproutSocial.Persistence/MappingProfiles/UserMapper.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using SproutSocial.Application.DTOs.UserDtos;
+using SproutSocial.Application.Features.Commands.AppUser.SelectTwoFaMethod;
+
+namespace SproutSocial.Persistence.MappingProfiles;
+
+public class UserMapper : Profile
+{
+    public UserMapper()
+    {
+        CreateMap<SelectTwoFaMethodDto, SelectTwoFaMethodCommandRequest>().ReverseMap();
+    }
+}
