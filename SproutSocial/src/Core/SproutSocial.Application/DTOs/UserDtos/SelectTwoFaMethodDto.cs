a9852fe379744889e1d2c758f5e6cfbbbd63d70d
Abdulaziz930 <abdulazizak@code.edu.az>
2023-05-21 13:10:56 +0400

    add select 2fa method dtos

---
+++ SproutSocial/src/Core/SproutSocial.Application/DTOs/UserDtos/SelectTwoFaMethodDto.cs
@@ -0,0 +1,3 @@
+﻿namespace SproutSocial.Application.DTOs.UserDtos;
+
+public record SelectTwoFaMethodDto(string UserId, string TwoFaMethodId);
---
+++ SproutSocial/src/Core/SproutSocial.Application/DTOs/UserDtos/SelectTwoFaMethodResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace SproutSocial.Application.DTOs.UserDtos;
+
+public record SelectTwoFaMethodResponseDto
+{
+    public HttpStatusCode StatusCode { get; init; }
+    public string? Message { get; init; }
+}
