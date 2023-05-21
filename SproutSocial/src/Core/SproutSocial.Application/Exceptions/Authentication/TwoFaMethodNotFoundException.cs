0a9ff8f03a8f702f99987dd133b76ea92beee687
Abdulaziz930 <abdulazizak@code.edu.az>
2023-05-21 13:11:39 +0400

    add 2fa method exceptions

---
+++ SproutSocial/src/Core/SproutSocial.Application/Exceptions/Authentication/TwoFaMethodNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿namespace SproutSocial.Application.Exceptions.Authentication;
+
+public sealed class TwoFaMethodNotFoundException : Exception, IServiceException
+{
+    public HttpStatusCode StatusCode => HttpStatusCode.NotFound;
+
+    public string ErrorMessage { get; }
+
+    public TwoFaMethodNotFoundException()
+    {
+        ErrorMessage = "2FA method not found";
+    }
+
+    public TwoFaMethodNotFoundException(string message) : base(message)
+    {
+        ErrorMessage = message;
+    }
+
+    public TwoFaMethodNotFoundException(string message, Exception? innerException) : base(message, innerException)
+    {
+        ErrorMessage = message;
+    }
+}
---
+++ SproutSocial/src/Core/SproutSocial.Application/Exceptions/Authentication/TwoFaNotEnabledException.cs
@@ -0,0 +1,23 @@
+﻿namespace SproutSocial.Application.Exceptions.Authentication;
+
+public sealed class TwoFaNotEnabledException : Exception, IServiceException
+{
+    public HttpStatusCode StatusCode => HttpStatusCode.BadRequest;
+
+    public string ErrorMessage { get; }
+
+    public TwoFaNotEnabledException()
+    {
+        ErrorMessage = "2FA not active in this account";
+    }
+
+    public TwoFaNotEnabledException(string message) : base(message)
+    {
+        ErrorMessage = message;
+    }
+
+    public TwoFaNotEnabledException(string message, Exception? innerException) : base(message, innerException)
+    {
+        ErrorMessage = message;
+    }
+}
